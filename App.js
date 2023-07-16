e9c6e6c35acbf9cd3b51044f88abaac62672ab0b
Rogelio Zamora Jr <dotenterface@gmail.com>
2023-07-16 11:28:22 -0500

    --Updates +folders, +fonts

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { useCallback } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    advent_light: require('./assets/fonts/AdventPro-Light.ttf'),
+    advent_regular: require('./assets/fonts/AdventPro-Regular.ttf'),
+    bellota_light: require('./assets/fonts/BellotaText-Light.ttf'),
+    bellota_regular: require('./assets/fonts/BellotaText-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Avenue Coffee Co.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +38,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontFamily: 'bellota_regular',
+    fontSize: 40
+  }
 });
